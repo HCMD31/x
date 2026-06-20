ad3787cfd9fee4f8c49513953be798161d255ed5
星雰客 <hcmd31@163.com>
2026-06-20 22:33:58 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,3 @@
+<?php
+/* 星空于梦【www.xki0.com】 */
+ echo "\xef\xbb\277"; session_start(); session_destroy(); header("\x4c\157\x63\141\164\x69\157\156\x3a\40\151\x6e\144\145\x78\x2e\x70\150\160"); exit;
